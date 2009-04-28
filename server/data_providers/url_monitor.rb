7b552f15082ec39c580bdbd3fc0d8b10258b54f4
Brenton Fletcher <bloopletech@guest-wl696.ce.unisa.edu.au>
2009-04-28 20:07:17 +0930

    Fix and enhance url monitor, bump version

--- server/data_providers/url_monitor.rb
+++ server/data_providers/url_monitor.rb
@@ -11,13 +11,19 @@ class DataProviders::UrlMonitor
     @thread = Thread.new do
       while(true)
         @settings[:urls].sort.each do |url|
+          @mutex.synchronize { @readings[url] = { :response_time => -1, :works => (@readings.key?(url) && @readings[url][:works] == :failed ? :failed : :waiting) } }
           duration = -1
-          works = false
+          works = :failed
           begin
+            uri = URI.parse(url)
             start = Time.now
-            result = Net::HTTP.get(URI.parse(url))
+            Net::HTTP.start(uri.host) { |http|
+              http.read_timeout = http.open_timeout = @settings[:danger_response_time_threshold] * 1000
+              raise Exception unless http.get(uri.path).code.include?(200, 201, 202, 203, 204, 205, 206, 301, 302, 304)
+            }
+
             duration = Time.now - start
-            works = true
+            works = :works
           rescue Exception => e
           end
           @mutex.synchronize { @readings[url] = { :response_time => duration * 1000, :works => works } }
@@ -32,8 +38,8 @@ class DataProviders::UrlMonitor
     out = {}
     @mutex.synchronize { out[:urls] = @readings.to_a.sort_by { |e| e[0] } }
     out[:urls].each do |(url, info)|
-      out[:status] = 'warning' if !info[:works] or info[:response_time] > @settings[:warning_response_time_threshold] and !out[:status] == 'danger'
-      out[:status] = 'danger' if !info[:works] or info[:response_time] > @settings[:danger_response_time_threshold]
+      out[:status] = 'warning' if (info[:works] == :failed) or info[:response_time] > @settings[:warning_response_time_threshold] and !out[:status] == 'danger'
+      out[:status] = 'danger' if (info[:works] == :failed) or info[:response_time] > @settings[:danger_response_time_threshold]
     end
     out
   end
@@ -45,7 +51,7 @@ for(var i = 0; i < data_source['urls'].length; i++)
 {
    var ud = data_source['urls'][i][1];
    temp += "<div class='major_figure'><span class='title'>" + data_source['urls'][i][0] + "</span><span class='figure'>" +
-    (!ud['works'] ? 'Failed</span>' : ud['response_time'] + "</span><span class='unit'>ms</span>") + "</div>";
+    (ud['works'] == 'failed' ? 'Failed</span>' : (ud['works'] == 'waiting' ? 'Waiting</span>' : ud['response_time'] + "</span><span class='unit'>ms</span>")) + "</div>";
 }
 
 sc.innerHTML = temp;
